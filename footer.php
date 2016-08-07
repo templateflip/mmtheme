498dbffff000aeecc082254487d9e8e3a1e78f40
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-08-07 15:50:24 +0530

    Removing sidebar from blog index page

--- footer.php
+++ footer.php
@@ -15,8 +15,8 @@
 
 	<footer id="colophon" class="site-footer container-content section-block text-center" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mmtheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mmtheme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mmtheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mmtheme' ), 'WordPress' ); ?>.</a>
+			<span class="sep"> </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mmtheme' ), 'mmtheme', '<a href="https://kkthemes.com/" rel="designer">Kanishk Kunal</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,7 @@
  * @package MMTheme
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( ! is_active_sidebar( 'sidebar-1' ) || is_home() ) {
 	return;
 }
 ?>
