46d12f307182db10c52fbfce422ef3dd35437de9
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-25 00:28:06 +0530

    Header and footer

--- footer.php
+++ footer.php
@@ -1,26 +1,11 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package MMTheme
- */
+</div><!-- #content -->
 
-?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer container-content section text-center" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mmtheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mmtheme' ), 'WordPress' ); ?>.</a>
-			<span class="sep"> </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mmtheme' ), 'mmtheme', '<a href="https://templateflip.com/" rel="designer">TemplateFlip</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+<footer class="footer container-content section text-center" role="contentinfo">
+  <div>
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mmtheme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mmtheme' ), 'WordPress' ); ?>.</a>
+    <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mmtheme' ), 'mmtheme', '<a href="https://templateflip.com/" rel="designer">TemplateFlip</a>' ); ?>
+  </div>
+</footer>
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -1,60 +1,55 @@
-<?php
-/**
- * The header for our theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package MMTheme
- */
-
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
-<?php wp_head(); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
+<body id="top" <?php body_class(); ?>>
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'mmtheme' ); ?></a>
 
-	<header id="header" class="site-header" role="banner">
-		<div  class="container-full">
-			<div class="site-branding header-left">
-				<?php
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif; ?>
-			</div><!-- .site-branding -->
-			<div class="header-right">
-				<?php get_search_form( true ); ?>
+	<header class="header" role="banner">
+		<div class="container">
+			<div class="header-left">
+        <a class="site-branding" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+          <?php if ( is_front_page() && is_home() ) : ?>
+            <h1 class="title"><?php bloginfo( 'name' ); ?></h1>
+				  <?php else : ?>
+            <span class="title"><?php bloginfo( 'name' ); ?></span>
+				  <?php endif; ?>
+        </a>
 			</div>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mmtheme' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
+      <div class="menu-toggle">
+        <input type="checkbox" id="menu-toggle">
+        <label class="header-right" for="menu-toggle">
+          <span class="css-icon-hamburger">
+            <span><?php esc_html_e( 'Toggle', 'mmtheme' ); ?></span>
+          </span>
+          <span><?php esc_html_e( 'Menu', 'mmtheme' ); ?></span>
+        </label>
+        <div class="menu-toggle-content">
+          <nav role="navigation">
+             <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+          </nav><!-- #site-navigation -->
+          <div class="header-right">
+            <?php get_search_form( true ); ?>
+          </div>
+        </div>
 		</div><!-- .container -->
 	</header><!-- #header -->
 	<?php
 	if ( is_front_page() && is_home() ) :
 		$description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
-				<div class="site-sub-header">
-					<div class="container-content section-hero">
-						<h2 class="site-description sub-heading h1"><?php echo $description; /* WPCS: xss ok. */ ?></h2>
+				<div class="sub-header section-medium highlight text-center">
+					<div class="container-content">
+						<h2 class="site-description h1"><?php echo $description; /* WPCS: xss ok. */ ?></h2>
 					</div>
 				</div>
 			<?php
 			endif;
 	endif; ?>
 
-	<div id="content" class="container-content">
+	<div id="content">
