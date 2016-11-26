7b66abdc66bfeeb75f56ec7875d0a8aa4dd7ffd8
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-26 22:57:56 +0530

    Footer text small

--- footer.php
+++ footer.php
@@ -7,9 +7,12 @@
     <div class="widget-area">
       <?php dynamic_sidebar( 'sidebar-footer' ); ?>
     </div>
-  <?php endif;
-  
-   printf( esc_html__( 'Built with %1$s theme for %2$s.', 'mmtheme' ), '<a href="https://templateflip.com/wordpress/mmtheme/" target="_blank">Minimal Modern</a>', '<a href="https://wordpress.org" target="_blank">WordPress</a>' ); ?>
+  <?php endif;?>
+
+  <p class="text-small">    
+    <?php 
+      printf( esc_html__( 'Built with %1$s theme for %2$s.', 'mmtheme' ), '<a href="https://templateflip.com/wordpress/mmtheme/" target="_blank">Minimal Modern</a>', '<a href="https://wordpress.org" target="_blank">WordPress</a>' ); ?>
+  </p>
   </div>
 </footer>
 
