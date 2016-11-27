5a3463f9ca0aec3eefd50bab220e12f861e1b295
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-27 20:46:49 +0530

    Widget titles

--- inc/setup.php
+++ inc/setup.php
@@ -55,9 +55,9 @@ add_action('after_setup_theme', function () {
 */
 add_action('widgets_init', function () {
     $config = [
-      'before_widget' => '<section class="widget section %1$s %2$s">',
+      'before_widget' => '<section class="widget %1$s %2$s">',
       'after_widget'  => '</section>',
-      'before_title'  => '<h3 class="h4 section-title">',
+      'before_title'  => '<h3 class="h4 widget-title">',
       'after_title'   => '</h3>'
     ];
     register_sidebar([
