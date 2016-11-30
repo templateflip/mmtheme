0675fe4646de6487bd20c90ed8afe4788dbc206f
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-30 14:43:21 +0530

    Post categories and tags in single

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,6 +9,28 @@
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
+    <div class="entry-meta">
+    <?php
+      /* translators: used between list items, there is a space after the comma */
+      $categories_list = get_the_category_list( __( ', ', 'mmtheme' ) );
+      
+      /* translators: used between list items, there is a space after the comma */
+      $tag_list = get_the_tag_list( '', __( ', ', 'mmtheme' ) );
+
+      $footer_meta = '';
+      if ( '' != $categories_list ) {
+         $footer_meta .= sprintf(__( 'Posted in: %1$s <br>', 'mmtheme' ), $categories_list);
+      } 
+
+      if ( '' != $tag_list ) {
+        $footer_meta .= sprintf(__( 'Tags: %1$s', 'mmtheme' ), $tag_list);
+      }
+
+      if( '' != $footer_meta) {
+        echo '<p>' . $footer_meta . '</p>';
+      }
+    ?>
+    </div>
     <footer>
       <?php wp_link_pages( array(
           'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'mmtheme' ),
